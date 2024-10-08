40e4f2b13b53ac327ae3e2d222bb7cb8dae2641a
David Tully <169837586+tullyd43@users.noreply.github.com>
2024-10-08 18:06:07 -0400

    Gameboard module functions created

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,7 @@
 // Player factory. Name is players name. Symbol is piece picked for game (X or O)
 const Player = (name, symbol) => {
 	let score = 0;
+
 	const getScore = () => {
 		return score;
 	};
@@ -8,8 +9,8 @@ const Player = (name, symbol) => {
 		return score++;
 	};
 	const resetScore = () => {
-		return score = 0
-	}
+		return (score = 0);
+	};
 	const details = () => {
 		return `${name} is playing with ${symbol}'s`;
 	};
@@ -19,19 +20,45 @@ const Player = (name, symbol) => {
 		getScore,
 		addPoint,
 		resetScore,
-        details,
+		details,
 	};
 };
 
+const player1 = Player("David", "X")
+console.log(player1.symbol)
+
 let Gameboard = (() => {
 	const gameBoard = [];
-	
+	const fillBoard = () => {
+		gameBoard.length = 9
+		return gameBoard.fill(null, 0, 9)
+	}
+	const resetGameBoard = () => {
+		return gameBoard.length = 0;
+	};
+	const addMove = (position, playerSymbol) => {
+		return gameBoard.splice(position, 1, playerSymbol)
+	}
 	return {
 		gameBoard,
+		fillBoard,
+		resetGameBoard,
+		addMove,
 	};
 })();
 
+Gameboard.fillBoard()
+console.log(Gameboard.gameBoard);
+Gameboard.addMove(6, player1.symbol);
+console.log(Gameboard.gameBoard);
+console.log(Gameboard.gameBoard.length);
+Gameboard.resetGameBoard();
+console.log(Gameboard.gameBoard)
+
+
+
 let Game = (() => {
+
 	// let play = prompt("Would you like to play a game of Tic Tac Toe?")
 	// if (play.toLowerCase() === "yes") {
 	//     console.log("Lets play!")
