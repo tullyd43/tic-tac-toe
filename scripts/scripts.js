82703b53f49ffe240aa0061b4b8bbb52811683d5
David Tully <169837586+tullyd43@users.noreply.github.com>
2024-10-09 16:19:34 -0400

    Game object created. Gameboard updated. Ui layout  implemented

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -24,53 +24,113 @@ const Player = (name, symbol) => {
 	};
 };
 
-const player1 = Player("David", "X")
-console.log(player1.symbol)
+const player1 = Player("David", "X");
 
 let Gameboard = (() => {
-	const gameBoard = [];
+	const gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+	const winningCombos = {
+		xWin: ["X", "X", "X"],
+		oWin: ["O", "O", "O"],
+		combo1() {
+			return gameBoard.slice(0, 3);
+		},
+		combo2() {
+			return gameBoard.slice(3, 6);
+		},
+		combo3() {
+			return gameBoard.slice(6, 9);
+		},
+		combo4() {
+			let x = [gameBoard[0], gameBoard[3], gameBoard[6]];
+			return x;
+		},
+		combo5() {
+			let x = [gameBoard[1], gameBoard[4], gameBoard[7]];
+			return x;
+		},
+		combo6() {
+			let x = [gameBoard[2], gameBoard[5], gameBoard[8]];
+			return x;
+		},
+		combo7() {
+			let x = [gameBoard[0], gameBoard[4], gameBoard[8]];
+			return x;
+		},
+		combo8() {
+			let x = [gameBoard[2], gameBoard[4], gameBoard[6]];
+			return x;
+		},
+	};
 	const fillBoard = () => {
-		gameBoard.length = 9
-		return gameBoard.fill(null, 0, 9)
-	}
+		gameBoard.length = 9;
+		return gameBoard.fill(null, 0, 9);
+	};
 	const resetGameBoard = () => {
-		return gameBoard.length = 0;
+		return (gameBoard.length = 0);
 	};
 	const addMove = (position, playerSymbol) => {
-		return gameBoard.splice(position, 1, playerSymbol)
-	}
+		return gameBoard.splice(position, 1, playerSymbol);
+	};
 	return {
 		gameBoard,
 		fillBoard,
 		resetGameBoard,
 		addMove,
+		winningCombos,
 	};
 })();
 
-Gameboard.fillBoard()
-console.log(Gameboard.gameBoard);
-Gameboard.addMove(6, player1.symbol);
-console.log(Gameboard.gameBoard);
-console.log(Gameboard.gameBoard.length);
-Gameboard.resetGameBoard();
-console.log(Gameboard.gameBoard)
-
-
-
 let Game = (() => {
+	const players = [];
+	// const winningCombos = {
 
-	// let play = prompt("Would you like to play a game of Tic Tac Toe?")
-	// if (play.toLowerCase() === "yes") {
-	//     console.log("Lets play!")
 	// }
+	const numPlayers = (x) => {
+		return (players.length = x);
+	};
+	const createPlayer = (playerName, symbol) => {
+		let player = Player(playerName, symbol);
+		return players.push(player);
+	};
+	const startGame = () => {
+		Gameboard.fillBoard();
+	};
+	const checkWinner = () => {
+		if (
+			Gameboard.winningCombos.combo1() ||
+			Gameboard.winningCombos.combo2() ||
+			Gameboard.winningCombos.combo3() ||
+			Gameboard.winningCombos.combo4() ||
+			Gameboard.winningCombos.combo5() ||
+			Gameboard.winningCombos.combo6() ||
+			Gameboard.winningCombos.combo7() ||
+			Gameboard.winningCombos.combo8()
+			===
+			Gameboard.winningCombos.xWin ||
+			Gameboard.winningCombos.oWin
+		) {
+		}
+	};
 	const newGame = () => {
 		// Code here
 	};
 	return {
+		players,
+		numPlayers,
+		createPlayer,
+		startGame,
+		checkWinner,
 		newGame,
+		// winningCombos
 	};
 })();
 
-// console.log(Game.gameBoard)
-// console.log(Game)
-// console.log(gameBoard)
+console.log(Gameboard.gameBoard);
+console.log(Gameboard.winningCombos.combo1());
+console.log(Gameboard.gameBoard);
+console.log(Gameboard.winningCombos.combo2());
+console.log(Gameboard.gameBoard);
+console.log(Gameboard.winningCombos.combo3());
+console.log(Gameboard.gameBoard);
+console.log(Gameboard.winningCombos.combo4());
+
